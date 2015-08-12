88271fb75a653bd03d96dfffeb97f5858549c15f
Mohit Chauhan <mss.chauhanmohit@gmail.com>
2015-08-12 10:58:20 +0530

    upload code with drag and drop functionality to git repo

--- public/lib/customScripts/controller/mainController.js
+++ public/lib/customScripts/controller/mainController.js
@@ -20,17 +20,16 @@ app.controller('mainController',['$scope','MapService','$http',function($scope,M
             MapService.geocodeAddress(address).then(function (location) {
                 var pos = {"latitude":location.G,"longitude":location.K} ;
                 $scope.location = pos ;
-                $scope.getDataFromApi($scope.limit) ;
+                $scope.getDataFromApi($scope.location.latitude,$scope.location.longitude,$scope.limit) ;
             });
         }
                 
-        $scope.getDataFromApi = function(limit){
+        $scope.getDataFromApi = function(lat,lang,limit){
 	    if (!limit) {
 		limit = 100 ;
 	    }
 	    $scope.showLoder = true ;
-            $http.get('/showData?lat='+$scope.location.latitude+'&lang='+$scope.location.longitude+'&limit='+limit)
-	   // $http.get('https://data.cityofchicago.org/resource/6zsd-86xi.json')
+            $http.get('/showData?lat='+lat+'&lang='+lang+'&limit='+limit)
             .success(function(res,status,config,header){
                 console.log("Data comes in success section");
                 $scope.shortlistedData = res ;
@@ -50,7 +49,7 @@ app.controller('mainController',['$scope','MapService','$http',function($scope,M
 		limit = 100
 	    }
 	    $scope.showLoder = true ;
-	    $http.get('/showData?lat=41.883811&lang=-87.631749&limit='+limit)
+	    $http.get('/showData?lat='+$scope.location.latitude+'&lang='+$scope.location.longitude+'&limit='+limit)
 	    .success(function(res,status,config,header){
                 console.log("Data comes in success section");
                 $scope.shortlistedData = res ;
@@ -59,4 +58,37 @@ app.controller('mainController',['$scope','MapService','$http',function($scope,M
                 console.log("Error comes in this section");
             });
 	}
+	
+	$scope.$on('mapInitialized', function(event, map) {
+
+		 google.maps.event.addListener(map, "dragend", function() {
+		 	var zoomLevel = map.getZoom();
+		 	var bounds = map.getBounds();
+		 	var ne = bounds.getNorthEast();
+		 	var sw = bounds.getSouthWest();
+		 	var d = getDistance(map.center, ne);
+			var loc = map.center ;	
+			$scope.location.latitude = loc.G;
+			$scope.location.longitude = loc.K;
+			$scope.limit = d ;
+			$scope.getDataFromApi($scope.location.latitude,$scope.location.longitude,$scope.limit) ;
+		});
+
+		var rad = function(x) {
+			return x * Math.PI / 180;
+		};
+
+		var getDistance = function(p1, p2) {
+			var R = 6378137; // Earth?s mean radius in meter
+			var dLat = rad(p2.G - p1.G);
+			var dLong = rad(p2.K - p1.K);
+			var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+				Math.cos(rad(p1.lat())) * Math.cos(rad(p2.lat())) *
+				Math.sin(dLong / 2) * Math.sin(dLong / 2);
+			var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+			var d = (R * c) ;
+			return d; // returns the distance in meters
+		}
+
+	});
 }]);
